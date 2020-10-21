af083c27983d683dd3c87145a65cfabac99b69a2
yahiachames <yahiachames@gmail.com>
2020-10-21 13:19:27 +0100

    persist store

--- App.js
+++ App.js
@@ -3,14 +3,20 @@ import Main from "./components/MainComponent";
 import { StyleSheet } from "react-native";
 
 import { Provider } from "react-redux";
+import { PersistGate } from 'redux-persist/es/integration/react'
+import { Loading } from './components/LoadingComponent';
 import { ConfigureStore } from "./redux/configureStore";
 
-const store = ConfigureStore();
+const { persistor, store } = ConfigureStore();
 
 export default function App() {
   return (
     <Provider store={store}>
+      <PersistGate 
+          loading={<Loading />}
+          persistor={persistor}>
       <Main />
+      </PersistGate>
     </Provider>
   );
 }
--- redux/configureStore.js
+++ redux/configureStore.js
@@ -1,4 +1,7 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
+import { persistStore, persistCombineReducers } from 'redux-persist';
+
+import AsyncStorage from '@react-native-community/async-storage';
 import thunk from "redux-thunk";
 import logger from "redux-logger";
 import { dishes } from "./dishes";
@@ -8,8 +11,13 @@ import { leaders } from "./leaders";
 import { favorites } from './favorites';
 
 export const ConfigureStore = () => {
+  const config = {
+    key: 'root',
+    storage : AsyncStorage,
+    debug: true
+  }
   const store = createStore(
-    combineReducers({
+    persistCombineReducers(config,{
       dishes,
       comments,
       promotions,
@@ -18,6 +26,6 @@ export const ConfigureStore = () => {
     }),
     applyMiddleware(thunk)
   );
-
-  return store;
+  const persistor = persistStore(store)
+  return { persistor, store };
 };
